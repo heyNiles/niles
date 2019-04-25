752862fffe46ce6ea1e0b5f7ebef29a31203b950
Gabi Dobocan <gabi@telepat.io>
2019-04-25 15:48:49 +0300

    Initial scheduled tasks support

---
+++ scheduler.js
@@ -0,0 +1,7 @@
+// const schedule = require('node-schedule');
+
+// const task = schedule.scheduleJob('4 * * * *', () => {
+//   console.log('The answer to life, the universe, and everything!');
+// });
+
+// TODO: Implement scheduler
